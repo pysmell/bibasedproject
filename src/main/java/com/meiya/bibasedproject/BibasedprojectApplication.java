775f8cd41a155ea4161632193879a8fa82fcc1fd
lqw <382394323@qq.com>
2017-11-22 14:31:53 +0800

    Initial commit

---
+++ src/main/java/com/meiya/bibasedproject/BibasedprojectApplication.java
@@ -0,0 +1,12 @@
+package com.meiya.bibasedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibasedprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibasedprojectApplication.class, args);
+	}
+}
